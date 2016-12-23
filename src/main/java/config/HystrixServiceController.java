34963367ebd9c5ee956e99312913f2c9f497576b
Matheus <matheus.goes@netshoes.com>
2016-12-23 14:45:44 -0200

    Fallback para rotas no zuul.

--- src/main/java/config/HystrixServiceController.java
+++ src/main/java/config/HystrixServiceController.java
@@ -1,14 +1,29 @@
 package config;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import config.model.UserRequest;
 
 @RestController
 public class HystrixServiceController {
 
-	@RequestMapping("/")
+	@RequestMapping("/generateMessage")
 	public String home() {
 		return "Hello!";
+	}
+
+	@RequestMapping(value = "/simulate")
+	public String simulate() {
+		throw new RuntimeException("HystrixServiceController Simulation Exception");
+	}
+
+	@RequestMapping(value = "/user", method = POST)
+	public String user(@RequestBody final UserRequest user) {
+		return user.toString();
 	}
 
 }
